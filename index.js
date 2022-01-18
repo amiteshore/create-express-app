4d19935519f7e52664c5261bb9546f1206fdc69c
Amit Eshore <amiteshore@gmail.com>
2022-01-18 14:31:33 +0530

    Add console logs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import path from "path";
 import fs from "fs";
 import url from "url";
+import chalk from "chalk";
 import { execSync } from "child_process";
 import { Command } from "commander";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const { green, cyan, bold } = chalk;
 
 let projectName;
 
@@ -37,6 +39,10 @@ let packageJson = {
     dev: "nodemon --watch .env --watch src --exec babel-node src"
   }
 };
+
+console.log();
+console.log(`Creating a new express app in ${green(projectPath)}.`);
+console.log();
 
 fs.mkdirSync(projectPath);
 
@@ -90,10 +96,25 @@ let command;
 
 command = `npm install --prefix ${projectPath} --save`;
 command += ` ${dependencies.join(" ")}`;
+
+console.log("Installing dependencies:");
+for (const dependency of dependencies) {
+  console.log(`- ${cyan(dependency)}`);
+}
+console.log();
+
 execSync(command, { stdio: "inherit" });
 
 command = `npm install --prefix ${projectPath} --save-dev`;
 command += ` ${devDependencies.join(" ")}`;
+
+console.log();
+console.log("Installing devDependencies:");
+for (const devDependency of devDependencies) {
+  console.log(`- ${cyan(devDependency)}`);
+}
+console.log();
+
 execSync(command, { stdio: "inherit" });
 
 process.chdir(projectPath);
@@ -101,3 +122,14 @@ execSync("git init", { stdio: "ignore" });
 execSync("git branch -M main", { stdio: "ignore" });
 execSync("git add -A", { stdio: "ignore" });
 execSync('git commit -m "Initial commit"', { stdio: "ignore" });
+
+console.log();
+console.log(bold(green("Success!")));
+console.log();
+console.log(
+  "Run the following commands to start the server in development mode:"
+);
+console.log();
+console.log(`   ${cyan("cd")} ${projectName}`);
+console.log(`   ${cyan("npm run dev")}`);
+console.log();
